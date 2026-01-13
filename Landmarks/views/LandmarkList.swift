aca1a09a056fd5f552e08bb466bd6258b64dc2e6
Muna Lugonvu <munalugonvu@gmail.com>
2026-01-13 15:17:54 +0300

    add landmark array to landmark list

--- Landmarks/views/LandmarkList.swift
+++ Landmarks/views/LandmarkList.swift
@@ -7,15 +7,21 @@
 
 import SwiftUI
 
+
 struct LandmarkList: View {
     var body: some View {
-        List {
-       LandmarkRow(Landmark: landmarks[0])
-       LandmarkRow(Landmark: landmarks[1])
-               }
+        //\.id means “use the id property of each landmark”.
+        //List Takes a data source (landmarks) and a row builder closure.
+        List(landmarks) {
+            // landmark in: A closure (like a map function) that builds a row for each landmark. TAKE IT LIKE A MAP FUNCTION , SO for each element in the landmarks array add to landmark row something like that
+            landmark in
+            LandmarkRow(Landmark: landmark)
+        }
     }
 }
+
 
 #Preview {
     LandmarkList()
 }
+
