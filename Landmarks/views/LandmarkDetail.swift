5838f7b0e49ddb9a674158da761f7c9d5d0a37a7
Muna Lugonvu <munalugonvu@gmail.com>
2026-01-20 18:32:40 +0300

    Add Navigation to the landmark list page

---
+++ Landmarks/views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Muna Lugonvu on 13/01/2026.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
