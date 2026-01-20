56d53a75173363262f4e26226290c7488c5d9011
Muna Lugonvu <munalugonvu@gmail.com>
2026-01-20 18:34:34 +0300

    Remve hard coded values and add variables

--- Landmarks/views/CircleImage.swift
+++ Landmarks/views/CircleImage.swift
@@ -9,15 +9,20 @@ import SwiftUI
 
 
 struct CircleImage: View {
+    var image: Image
+
+
     var body: some View {
-        Image("turtlerock").clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .overlay{Circle().stroke(.black,lineWidth: 4)
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
             }
-            .shadow(radius:7)
+            .shadow(radius: 7)
     }
 }
 
 
 #Preview {
-    CircleImage()
+    CircleImage(image: Image("turtlerock"))
 }
--- Landmarks/views/Mapview.swift
+++ Landmarks/views/Mapview.swift
@@ -8,18 +8,25 @@
 import SwiftUI
 import MapKit
 
-struct Mapview: View {
+
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+
+
     var body: some View {
-        Map(initialPosition: .region(region))
+        Map(position: .constant(.region(region)))
     }
-    private var region : MKCoordinateRegion {
+
+
+    private var region: MKCoordinateRegion {
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-                        span:MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
         )
     }
 }
 
+
 #Preview {
-    Mapview()
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
 }
